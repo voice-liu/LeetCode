1f0c227a266d6930ed1226efd1e6b96ac21ffd2b
liusheng <ls1016@outlook.com>
2022-04-15 21:07:44 +0800

    init

---
+++ src/main/java/Solution.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ *
+ */
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+return true;
+    }
+}
